fda0f4e9e714eb1d3a5d0e77827edf918ec29d7f
Yuri <yuriqpaiva@gmail.com>
2022-07-15 19:05:06 -0300

    Add sidebar menu Drawer for smaller screen sizes

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,6 @@
-import { Flex, useBreakpointValue } from "@chakra-ui/react";
+import { Flex, Icon, IconButton, useBreakpointValue } from "@chakra-ui/react";
+import { RiMenuLine } from "react-icons/ri";
+import { useSidebarDrawer } from "../../context/SidebarDrawerContext";
 import { Logo } from "./Logo";
 import { NotificationsNav } from "./NotificationsNav";
 import { Profile } from "./Profile";
@@ -7,6 +9,8 @@ import { SearchBox } from "./SearchBox";
 export function Header() {
   // Returns true if user has a large screen:
   const isWideVersion = useBreakpointValue({ base: false, lg: true });
+
+  const { onOpen } = useSidebarDrawer();
 
   return (
     <Flex
@@ -18,6 +22,16 @@ export function Header() {
       px="6"
       align="center"
     >
+      {!isWideVersion && (
+        <IconButton
+          aria-label="Open navigation"
+          icon={<Icon as={RiMenuLine} />}
+          fontSize="24"
+          variant="unstyled"
+          onClick={onOpen}
+          mr="2"
+        ></IconButton>
+      )}
       <Logo />
       {isWideVersion && <SearchBox />}
       <Flex align="center" ml="auto">
