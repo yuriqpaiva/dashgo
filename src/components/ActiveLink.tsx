fb7e429f37437e35168ecd73ca6d0cdc57901b3b
Yuri <yuriqpaiva@gmail.com>
2022-07-16 14:51:18 -0300

    Add Active link color

---
+++ src/components/ActiveLink.tsx
@@ -0,0 +1,38 @@
+import Link, { LinkProps } from "next/link";
+import { useRouter } from "next/router";
+import { cloneElement, ReactElement } from "react";
+
+interface ActiveLinkProps extends LinkProps {
+  // Just React Components as children (not html tags)
+  children: ReactElement;
+  shouldMatchExactHref?: boolean;
+}
+
+export function ActiveLink({
+  children,
+  shouldMatchExactHref = false,
+  ...rest
+}: ActiveLinkProps) {
+  const { asPath } = useRouter();
+  console.log(asPath);
+
+  let isActive = false;
+
+  if (shouldMatchExactHref && (asPath === rest.href || asPath === rest.as)) {
+    isActive = true;
+  }
+
+  if (
+    (!shouldMatchExactHref && asPath.startsWith(String(rest.href))) ||
+    asPath.startsWith(String(rest.as))
+  ) {
+    isActive = true;
+  }
+
+  return (
+    <Link {...rest}>
+      {/* Pass property to children Element */}
+      {cloneElement(children, { color: isActive ? "pink.400" : "gray.50" })}
+    </Link>
+  );
+}
