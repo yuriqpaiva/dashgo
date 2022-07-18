bd03aea47cca3c8e175e77b26c81fa2e0283ad7c
Yuri <yuriqpaiva@gmail.com>
2022-07-18 15:53:12 -0300

    Use react query devtools

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -23,14 +23,29 @@ import { Sidebar } from "../../components/Sidebar";
 import { useQuery } from "react-query";
 
 export default function UserList() {
-  const { data, isLoading, error } = useQuery("users", async () => {
-    const response = await fetch("http://localhost:3000/api/users");
-    const data = response.json();
+  const { data, isLoading, error } = useQuery(
+    "users",
+    async () => {
+      const response = await fetch("http://localhost:3000/api/users");
+      const data: any = await response.json();
 
-    return data;
-  });
+      const users = data.users.map((user: any) => {
+        return {
+          id: user.id,
+          name: user.name,
+          email: user.email,
+          createdAt: new Date(user.createdAt).toLocaleDateString("pt-BR", {
+            day: "2-digit",
+            month: "long",
+            year: "numeric",
+          }),
+        };
+      });
 
-  // console.log(query);
+      return users;
+    },
+    { staleTime: 1000 * 5 }
+  );
 
   const isWideVersion = useBreakpointValue({ base: false, lg: true });
 
@@ -83,50 +98,24 @@ export default function UserList() {
                   </Tr>
                 </Thead>
                 <Tbody>
-                  <Tr>
-                    <Td px={["4", "4", "6"]}>
-                      <Checkbox colorScheme="pink" />
-                    </Td>
-                    <Td>
-                      <Box>
-                        <Text fontWeight="bold">Yuri Paiva</Text>
-                        <Text fontSize="sm" color="gray.300">
-                          yuriqpaiva@gmail.com
-                        </Text>
-                      </Box>
-                    </Td>
-                    {isWideVersion && <Td>04 de Abril, 1999</Td>}
-                  </Tr>
-
-                  <Tr>
-                    <Td px={["4", "4", "6"]}>
-                      <Checkbox colorScheme="pink" />
-                    </Td>
-                    <Td>
-                      <Box>
-                        <Text fontWeight="bold">Yuri Paiva</Text>
-                        <Text fontSize="sm" color="gray.300">
-                          yuriqpaiva@gmail.com
-                        </Text>
-                      </Box>
-                    </Td>
-                    {isWideVersion && <Td>04 de Abril, 1999</Td>}
-                  </Tr>
-
-                  <Tr>
-                    <Td px={["4", "4", "6"]}>
-                      <Checkbox colorScheme="pink" />
-                    </Td>
-                    <Td>
-                      <Box>
-                        <Text fontWeight="bold">Yuri Paiva</Text>
-                        <Text fontSize="sm" color="gray.300">
-                          yuriqpaiva@gmail.com
-                        </Text>
-                      </Box>
-                    </Td>
-                    {isWideVersion && <Td>04 de Abril, 1999</Td>}
-                  </Tr>
+                  {data.map((user: any) => {
+                    return (
+                      <Tr key={user.id}>
+                        <Td px={["4", "4", "6"]}>
+                          <Checkbox colorScheme="pink" />
+                        </Td>
+                        <Td>
+                          <Box>
+                            <Text fontWeight="bold">{user.name}</Text>
+                            <Text fontSize="sm" color="gray.300">
+                              {user.email}
+                            </Text>
+                          </Box>
+                        </Td>
+                        {isWideVersion && <Td>{user.createdAt}</Td>}
+                      </Tr>
+                    );
+                  })}
                 </Tbody>
               </Table>
 
